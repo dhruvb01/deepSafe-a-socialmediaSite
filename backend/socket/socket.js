01ea3efd1a80d44e9caefb02d43415ebaab0d94e
dhruvb01 <dhruvbhardwaj0001@gmail.com>
2024-09-24 18:52:40 +0530

    h

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors:{
-        origin:'*',
+        origin:'https://deepsafe-a-socialmediasite-1.onrender.com',
         methods:['GET','POST']
     }
 })
